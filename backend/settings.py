9bd866a8f44d30058de4be96e7bb99262415b7b5
Tjiaz <tunjiazeez24@gmail.com>
2024-06-04 11:11:24 +0100

    added procfile

--- backend/settings.py
+++ backend/settings.py
@@ -77,6 +77,8 @@ TEMPLATES = [
 WSGI_APPLICATION = "backend.wsgi.application"
 
 
+
+
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -116,7 +118,10 @@ else:
         }
     }
 
+# import dj_database_url
+# import django_heroku
 
+# DATABASES['default']= dj_database_url.config(conn_max_age=600,ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
