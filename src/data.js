a7fbde52d30413c37abb316af76dc12ef6a905e5
Tjiaz <tunjiazeez24@gmail.com>
2024-06-04 18:25:06 +0100

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,30 @@
+// data.js
+import { FaSquareXTwitter } from "react-icons/fa6";
+const teamData = [
+  {
+    id: 1,
+    imgSrc: "./hands1.jpeg",
+    title: "Joshua’s SDR Journey",
+    description:
+      "Hi, my name is Joshua, I’m 10 years old and live in Dumfriesshire with mum, dad and sister Edith. Just4Childen needs your help to raise money for a life changing operation called SDR to help my independence and mobility. Joshua is 10 years old and lives near Canonbie in Dumfriesshire. He was born 2 months […]",
+    buttonText: "Read more",
+  },
+  {
+    id: 2,
+    imgSrc: "./hands2.jpeg",
+    title: "Scarlett’s Journey",
+    description:
+      "Just4Children are fundraising for Scarlett to receive specialist equipment and therapies and to make adaptions to her home with a wet room, stairlift and bigger bedroom to make her day to day life a little easier. Scarlett is 4 years old from Boston in Lincolnshire. Scarlett was diagnosed with epilepsy when she was 2 and […]",
+    buttonText: "Read more",
+  },
+  {
+    id: 3,
+    imgSrc: "./hands3.jpeg",
+    title: "Ruben’s Journey",
+    description:
+      "Just4Children are wanting to raise funds for Ruben to receive physiotherapy, specialist equipment, hydrotherapy and other therapies to help him become more independent and to reach his full potential in life.  Ruben is 2 year old little boy from Boston in Lincolnshire who made a quick appearance at 30 weeks and went into respiratory arrest. […]",
+    buttonText: "Read more",
+  },
+];
+
+export default teamData;
