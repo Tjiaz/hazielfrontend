4806aabf86c5b5f6054fe1541294bf3320851cbd
Tjiaz <tunjiazeez24@gmail.com>
2024-06-04 11:22:42 +0100

    changed db from localhost to 127.0.0.1

--- backend/settings.py
+++ backend/settings.py
@@ -110,7 +110,7 @@ else:
             "NAME": 'Hazel',
             'USER': 'Ola',
             'PASSWORD': 'Ola',
-            'HOST': 'localhost',
+            'HOST': '127.0.0.1',
             'PORT': '3306',
             'OPTIONS': {
                 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
